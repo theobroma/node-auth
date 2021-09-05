ae0513c718dfc51a1e512dbfa20b309bef50257c
Alexandr Siryj <theobroma222@gmail.com>
2021-09-05 06:11:49 +0300

    Implement logout and register, add middleware

--- api/src/validation.ts
+++ api/src/validation.ts
@@ -1,5 +1,9 @@
 import { celebrate, SchemaOptions, Segments, Joi } from "celebrate";
 
+const email = Joi.string().email().required();
+const name =Joi.string().required();
+const password = Joi.string().max(256).required(); // TODO password strength
+
 export const validate = (schema: SchemaOptions) =>
   celebrate(schema, {
     abortEarly: false,
@@ -7,7 +11,15 @@ export const validate = (schema: SchemaOptions) =>
 
 export const loginSchema = {
   [Segments.BODY]: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().max(256).required(),
+    email,
+    password,
   }),
+};
+
+export const registerSchema = {
+  [Segments.BODY]: Joi.object().keys({
+    email,
+    password,
+    name,
+  })
 };
